f9dd8d99863dcda90b71eebac8e35db4ffb8e7d6
Selvarion <50030834+Selvarion@users.noreply.github.com>
2020-09-12 10:19:06 +0200

    Add files via upload

---
+++ ChangeValues/src/IncrementAndDecrement.java
@@ -0,0 +1,19 @@
+public class IncrementAndDecrement {
+    public static void main(String[] args) {
+
+        int x = 1;
+        int y = 2;
+
+        while(x<=1){
+            x++;
+
+        }
+        while(y>=2){
+            y--;
+
+        }
+
+        System.out.println("Incremented x: " + x);
+        System.out.println("Decremented y: " + y);
+    }
+}
